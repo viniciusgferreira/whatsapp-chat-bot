c606cab0fef0f493a6b3dd39a699601882db856a
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-27 18:34:12 -0300

    html response to root endpoint

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => res.send('Hello, server is running.'))
+
 app.listen(3000, function () {
     console.log(`Server running on http://localhost:${port}`);
 })
