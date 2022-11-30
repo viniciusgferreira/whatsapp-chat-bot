5ba167d3aa89a9112caa18acb33b75ed4d82e6ab
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-30 20:55:44 -0300

    fix: better logs for runtime

--- server.js
+++ server.js
@@ -45,28 +45,33 @@ async function handleMessage(message) {
             if (message.hasMedia) {
                 const media = await message.downloadMedia();
                 media.filename = 'sticker';
+                console.log('Creating Sticker from image received');
 
                 //CREATE FOLDER UPLOAD
                 if (!fs.existsSync('./upload')) {
+                    console.log('creating ./upload folder')
                     fs.mkdirSync('./upload');
                 }
 
                 const mediaPath = './upload/';
                 const fullFilename = mediaPath + media.filename + '.jpg';
-                console.log(fullFilename);
 
                 try {
                     fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' });
-                    console.log('File downloaded successfully!', fullFilename);
-                    console.log(fullFilename);
+                    console.log('Image file downloaded successfully!', fullFilename);
                     MessageMedia.fromFilePath(filePath = fullFilename)
                     client.sendMessage(message.from, new MessageMedia(media.mimetype, media.data, media.filename), { sendMediaAsSticker: true, stickerAuthor: "Created By Bot", stickerName: "Stickers" })
+                    console.log('Sticker created and sent back to user.')
                     fs.unlinkSync(fullFilename)
-                    console.log(`File Deleted successfully!`,);
+                    console.log(`Image file Deleted successfully!`,);
                 } catch (err) {
                     console.log('Failed to save the file:', err);
-                    console.log(`File Deleted successfully!`,);
+                    console.log(`Image file Deleted successfully!`,);
                 }
+
+                // DELETE FOLDER 'UPLOAD'
+                fs.rmdirSync('./upload');
+                console.log('upload folder deleted.')
 
             }
     }
