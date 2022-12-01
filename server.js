0277692310b15d772eb34f72dc5f1a4e1adc241a
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-01 06:34:05 -0300

    refactor of unused code

--- server.js
+++ server.js
@@ -2,79 +2,70 @@ const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
-// Path where the session data will be stored
-const SESSION_FILE_PATH = './session.json';
-
-// Load the session data if it has been previously saved
-let sessionData;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
-
 // Use the saved values
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
 });
 
-//QR CODE
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+// QR CODE
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
 // Save session values to the file upon successful auth
 client.on('authenticated', () => {
-    console.log('Authenticated');
-
+  console.log('Authenticated');
 });
-
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
-client.on('message_create', handleMessage);
-
 async function handleMessage(message) {
-    switch (message.body) {
-        case '!menu':
-            console.log("incoming message: " + message.body);
-            message.reply('!fig - Envie junto com a foto (video/gifs ainda não) para criar uma figurinha\n\n!menu - Para ver opções disponíveis\n\n Bot Vagabundo - feito por Vinicius G. Ferreira');
-            break;
-        case '!fig':
-            console.log("incoming message: " + message.body);
-            if (message.hasMedia) {
-                const media = await message.downloadMedia();
-                media.filename = 'sticker';
-                console.log('Creating Sticker from image received');
+  switch (message.body) {
+    case '!menu':
+      console.log(`incoming message: ${message.body}`);
+      message.reply('!fig - Envie junto com a foto (video/gifs ainda não) para criar uma figurinha\n\n!menu - Para ver opções disponíveis\n\n Bot Vagabundo - feito por Vinicius G. Ferreira');
+      break;
+    case '!fig':
+      console.log(`incoming message: ${message.body}`);
+      if (message.hasMedia) {
+        const media = await message.downloadMedia();
+        media.filename = 'sticker';
+        console.log('Creating Sticker from image received');
 
-                //CREATE FOLDER UPLOAD
-                if (!fs.existsSync('./upload')) {
-                    console.log('creating ./upload folder')
-                    fs.mkdirSync('./upload');
-                }
+        // CREATE FOLDER UPLOAD
+        if (!fs.existsSync('./upload')) {
+          console.log('creating ./upload folder');
+          fs.mkdirSync('./upload');
+        }
 
-                const mediaPath = './upload/';
-                const fullFilename = mediaPath + media.filename + '.jpg';
+        const mediaPath = './upload/';
+        const fullFilename = `${mediaPath + media.filename}.jpg`;
 
-                try {
-                    fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' });
-                    console.log('Image file downloaded successfully!', fullFilename);
-                    MessageMedia.fromFilePath(filePath = fullFilename)
-                    client.sendMessage(message.from, new MessageMedia(media.mimetype, media.data, media.filename), { sendMediaAsSticker: true, stickerAuthor: "Created By Bot", stickerName: "Stickers" })
-                    console.log('Sticker created and sent back to user.')
-                    fs.unlinkSync(fullFilename)
-                    console.log(`Image file Deleted successfully!`,);
-                } catch (err) {
-                    console.log('Failed to save the file:', err);
-                    console.log(`Image file Deleted successfully!`,);
-                }
+        try {
+          fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' });
+          console.log('Image file downloaded successfully!', fullFilename);
+          MessageMedia.fromFilePath(fullFilename);
+          client.sendMessage(message.from, new MessageMedia(media.mimetype, media.data, media.filename), { sendMediaAsSticker: true, stickerAuthor: 'Created By Bot', stickerName: 'Stickers' });
+          console.log('Sticker created and sent back to user.');
+          fs.unlinkSync(fullFilename);
+          console.log('Image file Deleted successfully!');
+        } catch (err) {
+          console.log('Failed to save the file:', err);
+          console.log('Image file Deleted successfully!');
+        }
 
-                // DELETE FOLDER 'UPLOAD'
-                fs.rmdirSync('./upload');
-                console.log('upload folder deleted.')
+        // DELETE FOLDER 'UPLOAD'
+        fs.rmdirSync('./upload');
+        console.log('upload folder deleted.');
+      }
 
-            }
-    }
+      break;
+    default:
+  }
 }
+
+client.on('message_create', handleMessage);
 
 client.initialize();
